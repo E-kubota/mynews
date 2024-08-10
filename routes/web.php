5e5b735ea0d0d7f80fb9042c67e31217bf0ad597
e.Kubota <e.kubota.work@gmail.com>
2024-08-10 17:54:50 +0900

    Routeを設定

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\SelfProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(NewsController::class)->prefix('admin')->name('new.')->group(function() {
+    Route::get('news/create', 'add')->name('add');
+});
+
+Route::controller(SelfProfileController::class)->prefix('admin')->name('profile.')->group(function() {
+    Route::get('profile/create', 'add')->name('add');
+    Route::get('profile/edit', 'edit')->name('edit');
+});
+
+// 単純な書き方の場合
+// Route::get('admin/news/create', [NewsController::class, 'add']);
