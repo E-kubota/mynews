ebe9a01002ef0fcdcc0b5c26f8ae4f516af57c13
e.Kubota <e.kubota.work@gmail.com>
2024-08-10 18:23:13 +0900

    viewの設定

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\SelfProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\NewsController;
+use App\Http\Controllers\Admin\SelfProfileController;
 
 /*
 |--------------------------------------------------------------------------
