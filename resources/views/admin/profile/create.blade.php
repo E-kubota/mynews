b2d9cdd14636153911413413ef06641e950afdaf
e.Kubota <e.kubota.work@gmail.com>
2024-08-11 09:55:26 +0900

    vitaの設定とレイアウト切り出し

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>selfProfile create</h1>
-</body>
-</html>
+@extends('layouts.profile')
+
+@section('title', 'プロフィール作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
